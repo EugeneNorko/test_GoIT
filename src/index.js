66d37d77a579410b5903f934aef28ccc1ed985ae
EugeneNorko <zhenia020221@gmail.com>
2023-04-18 16:36:04 +0300

    fixBaseurl

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test_goit">
+    <BrowserRouter basename="test_goit/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
