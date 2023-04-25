99b3179b83dfe5ec5019231d3613d8e870eba755
EugeneNorko <zhenia020221@gmail.com>
2023-04-25 19:33:24 +0300

    ADD:mediaQueries

--- src/components/container/Container.styled.js
+++ src/components/container/Container.styled.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
-  width: 1280px;
+  width: 400px;
   padding: 0 15px 0 15px;
   margin: 0 auto 0 auto;
+
+  @media screen and (max-width: 767px) {
+    justify-content: center;
+  }
+  @media screen and (min-width: 840px) {
+    width: 840px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
+  }
 `;
 
 export default StyledContainer;
